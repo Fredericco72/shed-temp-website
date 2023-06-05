cf4f9062dd9b9ed8981e742ce8ca066d2db5c942
Fred <33041227+Fredericco72@users.noreply.github.com>
2023-06-05 09:49:39 +0100

    d3 testing

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from datetime import datetime
 from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
@@ -15,6 +15,10 @@ message = "Waiting for data"
 @app.route('/')
 def show_temp():
     return message
+
+@app.route("/chart")
+def chart():
+    return render_template("chart.html")
 
 @app.route("/data")
 def data():
---
+++ static/chart.js
@@ -0,0 +1,17 @@
+// set the dimensions and margins of the graph
+const width = 960,
+	height = 500;
+const svg = d3.select("#d3_demo").attr("width", width).attr("height", height);
+
+const x_scale = d3.scaleBand().range([0, width]).padding(0.1);
+const y_scale = d3.scaleLinear().range([height, 0]);
+
+console.log("Hello");
+
+d3
+	.json("http://shedtemp.pythonanywhere.com/data")
+	.then((data) => {
+		console.log("world");
+		console.log(data);
+	})
+	.catch((d) => console.log(d));
